835ca61230b70749e8a7e595a8984fcc2d6ffe27
Kyle Koeller <kjkoeller@bsu.edu>
2025-04-20 17:57:14 -0400

    Update __init__.py

--- glyphx/__init__.py
+++ glyphx/__init__.py
@@ -32,7 +32,7 @@ from .lmplot import lmplot
 
 # Version
 from .version import __version__
-__version__ = version.__version__
+# __version__ = version.__version__
 
 __all__ = [
     "Figure",
@@ -53,7 +53,7 @@ __all__ = [
     "ViolinPlotSeries",
     "SwarmPlotSeries",
     "CountPlotSeries",
-    "face_plot",
+    "facet_plot",
     "pairplot",
     "jointplot",
     "lmplot"
