32d3e09005154a7d80496e8f89a450a9725b8232
Kyle Koeller <kjkoeller@bsu.edu>
2025-04-20 03:40:15 -0400

    Create __init__.py

---
+++ glyphx/__init__.py
@@ -0,0 +1,31 @@
+from .figure import Figure
+from .layout import Axes, grid
+from .themes import themes
+from .utils import normalize
+
+# Core plotting interface
+from .plot import plot
+
+# Base chart types
+from .series import (
+    LineSeries,
+    BarSeries,
+    ScatterSeries,
+    PieSeries,
+    DonutSeries,
+    HistogramSeries,
+    HeatmapSeries,
+    BoxPlotSeries
+)
+
+# Advanced chart types
+from .grouped_bar import GroupedBarSeries
+from .violin_plot import ViolinPlotSeries
+from .swarm_plot import SwarmPlotSeries
+from .count_plot import CountPlotSeries
+
+# Seaborn-style composite plots
+from .facet_plot import facet_plot
+from .pairplot import pairplot
+from .jointplot import jointplot
+from .lmplot import lmplot
