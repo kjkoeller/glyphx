7f45b6291eb7b838ecab8e6109979a902d240a56
Kyle Koeller <kjkoeller@bsu.edu>
2026-04-07 19:55:42 -0400

    Documentation updates

--- docs/conf.py
+++ docs/conf.py
@@ -1,21 +1,37 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath(’..’))
 
-project = 'glyphx'
-author = 'Kyle Koeller'
-release = '1.2.0'
+project = ‘glyphx’
+author  = ‘Kyle Koeller’
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.todo',
-    'sphinx.ext.githubpages',
+‘sphinx.ext.autodoc’,
+‘sphinx.ext.napoleon’,
+‘sphinx.ext.viewcode’,
+‘sphinx.ext.todo’,
+‘sphinx.ext.githubpages’,
+‘sphinx.ext.intersphinx’,
 ]
 
-templates_path = ['_templates']
-exclude_patterns = []
+intersphinx_mapping = {
+‘python’: (‘https://docs.python.org/3’, None),
+‘numpy’:  (‘https://numpy.org/doc/stable’, None),
+‘pandas’: (‘https://pandas.pydata.org/docs’, None),
+}
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+autodoc_default_options = {
+‘members’:         True,
+‘undoc-members’:   False,
+‘show-inheritance’: True,
+}
+
+templates_path   = [’_templates’]
+exclude_patterns = [’_build’]
+
+html_theme = ‘sphinx_rtd_theme’
+html_static_path = [’_static’]
+html_theme_options = {
+‘navigation_depth’: 4,
+‘titles_only’:      False,
+}
